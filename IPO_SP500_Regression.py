f96cb1c1c4227f1b185a403977422c2a8e46fb40
Kelly <32198885+lkelly29@users.noreply.github.com>
2020-09-05 16:26:33 -0400

    Add files via upload

---
+++ IPO_SP500_Regression.py
@@ -0,0 +1,34 @@
+'''
+@author Kelly Lewis
+9/5/2020
+
+Linear Regression on IPO Data using year IPO went public and number of Days the
+stock had a higher percentage change than the S&P500
+
+Data Download:
+https://www.kaggle.com/proselotis/financial-ipo-data (1995 - 2016)
+ '''
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('C:\\Users\\Kelly\\Desktop\\Econometric Projects\\IPODatafull.csv')
+
+#print(df.head())
+
+daysbetter_per_year = df.groupby('Year').DaysBetterThanSP.mean().reset_index()
+X = daysbetter_per_year['Year']
+X = X.values.reshape(-1,1)
+
+y = daysbetter_per_year['DaysBetterThanSP']
+
+line_fitter = LinearRegression()
+line_fitter.fit(X,y)
+y_predict = line_fitter.predict(X)
+
+plt.scatter(X, y)
+plt.xlabel("Year stock went public")
+plt.ylabel("Average number of days where % change was higher than S&P 500")
+plt.plot(X,y_predict)
+plt.show()
